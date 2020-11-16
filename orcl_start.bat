123f8df734b8ffb687f264a326348c23d2583699
jyp <quswhddbs85@gmail.com>
2020-11-16 18:52:09 +0900

    commit

---
+++ orcl_start.bat
@@ -0,0 +1,5 @@
+net start OracleDBConsoleorcl
+net start OracleMTSRecoveryService
+net start OracleOraDb11g_home1TNSListener
+net start OracleServiceORCL
+isqlplusctl start
---
+++ orcl_stop.bat
@@ -0,0 +1,5 @@
+net stop OracleDBConsoleorcl
+net stop OracleMTSRecoveryService
+net stop OracleOraDb11g_home1TNSListener
+net stop OracleServiceORCL
+isqlplusctl stop
